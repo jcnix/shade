725472457fdbabae829f46ce81d5fe9a4cf70019
Casey Jones <jonescaseyb@gmail.com>
2010-12-29 23:27:04 -0600

    Don't need the my_user context anymore
    I'm using Django's auth system now which already has it's own context

--- social/contexts.py
+++
@@ -1,9 +0,0 @@
-from shade.social.models import User
-
-def my_user(request):
-    try:
-        my_user = request.session['user']
-        return {'my_user': my_user}
-    except KeyError:
-        return {}
-
