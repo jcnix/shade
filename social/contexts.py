5fd4e0d9d1d50e54c71c22479fde215865365979
Casey Jones <jonescaseyb@gmail.com>
2011-01-04 23:51:24 -0600

    Prevent 500 error when user isn't logged in.

--- social/contexts.py
+++ social/contexts.py
@@ -1,7 +1,10 @@
 from shade.social.models import Message
 
 def unread_message_count(request):
-    u = request.user
-    m = Message.objects.filter(recipient=u, read=False).count()
+    if request.user.is_authenticated():
+        u = request.user
+        m = Message.objects.filter(recipient=u, read=False).count()
+    else:
+        m = None
     return {'unread_msg_count': m}
 
