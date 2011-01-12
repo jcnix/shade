9e53758609f8c988ff16f5e6aa3b2dbe75131aed
Casey Jones <jonescaseyb@gmail.com>
2011-01-12 14:02:04 -0600

    Show list of upcoming events in sidebar

--- social/contexts.py
+++ social/contexts.py
@@ -1,4 +1,14 @@
 from shade.social.models import Message
+import datetime
+
+def upcoming_events(request):
+    if request.user.is_authenticated():
+        now = datetime.datetime.now()
+        user = request.user
+        events = user.get_profile().events.filter(datetime__gte=now)
+    else:
+        events = None
+    return {'upcoming_events': events}
 
 def unread_message_count(request):
     if request.user.is_authenticated():
