61f5bac9bf82c2fe5a70a0c2c2ce17c9dca17e8e
Casey Jones <jonescaseyb@gmail.com>
2011-03-10 20:57:15 +0000

    Do some real tests.

--- social/tests.py
+++ social/tests.py
@@ -6,18 +6,35 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib import auth
+from django.contrib.auth.models import User
+from shade.social.models import Message
+import datetime
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class MessagingTest(TestCase):
+    def setUp(self):
+        now = datetime.datetime.now()
+        self.client = Client()
+        self.user = User.objects.create_user('test', 'test@test.com',
+                'testpassword')
+        self.client.login(username='test', password='testpassword')
+        self.message = Message(author=self.user, recipient=self.user,
+                subject="Test", body="Test", read=False, sent=now)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+    def test_inbox(self):
+        response = self.client.get('/inbox/')
+        self.failUnlessEqual(response.status_code, 200)
 
->>> 1 + 1 == 2
-True
-"""}
+    def msg_view_test(self):
+        response = self.client.get('/inbox/view/'+self.message.id)
+        self.failUnlessEqual(response.status_code, 200)
+
+    def msg_compose_test(self):
+        response = self.client.get('/inbox/compose/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def msg_delete_test(self):
+        response = self.client.get('/inbox/delete'+self.message.id)
+        self.failUnlessEqual(response.status_code, 200)
 
